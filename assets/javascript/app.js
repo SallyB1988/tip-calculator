3a4df7621a2bfd29725a206fb73b5da75191f945
Sally Beckwith <sallypbeckwith@gmail.com>
2019-06-22 22:28:11 -0400

    Changed tip option to use radio buttons with common tip percentages and an option to add specific value

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,18 @@
 // REGEX patterns used to validate input
-  const regex_currency = /^((\d+)(\.\d{2})?)$/;
-  const regex_float = /^\d+(\.\d*)?$/;
-
-  window.onload = () => {
-// not sure I need anything here
-  }
-
+  const regex_currency = /^((\d+)(\.\d{2})?)$/; // positive float with max of 2 decimal places
+  const regex_float = /^\d+(\.\d*)?$/;  // positive float value
 
   $("#submit").on("click", (e) => {
     e.preventDefault();
-    // get values from form. Trim empty spaces off string. Remove leading zeroes
+    // get values from form. Trim empty spaces off string. Remove leading zeroes, if any
     let cost = $("#cost").val().trim();
     console.log(cost);
     if (cost[0] === ".") cost = "0"+cost;
     console.log('updated cost: '+cost);
-    let tipPercent = $("#tip").val().trim();
+    let tipPercent = $("#tipSelect input:radio:checked").val()
+    if (tipPercent === "other") {
+      tipPercent = $("#tip").val().trim();
+    }
     if (tipPercent[0] === ".") tipPercent = "0"+tipPercent;
     let split = $("#split").val().trim();
     let tipValue = 0;
